4ac88b8c922682fbdaf2cacc5d573a469f949abe
John Cobbs <johnjosephcobbs@gmail.com>
2021-12-06 14:45:12 -0800

    update

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,7 +2,7 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license !== "None") {
-    return `![Github license](https://img.shields.io/badge/license-${liecense}-blue.svf)`;
+    return `![Github license](https://img.shields.io/badge/license-${license}-blue.svf)`;
   }
   return "";
 }
@@ -10,18 +10,77 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license!== "None") {
-    return `/n* [Liecnse] (#liecnese)\n`;`
+  if (license !== "None") {
+    return `\n* [Liecnse] (#liecnese)\n`;
   }
+  return "";
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return `## License
+    This project is licensed under the${license} license.`;
+  }
+  return ";";
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+  
+  ${data.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+
+  *[Usage](#usage)
+  ${renderLicenseLink(data.license)}
+  *[Contriguiting](#contributing)
+
+  *[Contributing](#contributing)
+
+  * [Tests](#tests)
+
+  * [Questions](#questions)
+
+  ## Installation
+
+  To install necessary dependencies, run the following command:
+
+  \`\`\`
+  ${data.installation}
+  \`\`\`
+
+  ##Usage
+
+  ${data.usage}
+
+  ${renderLicenseSection(data.license)}
+
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Tests
+
+  To run tests, run the following command:
+
+  \`\`\`
+  ${data.test}
+  \`\`\`
+
+  ## Questions
+
+  If you have any questions about the repo, open an issue or contact me directly at ${
+    data.email
+  }. You can find more of my work at [${data.github.com}](https://github.com/${
+    data.github
+  }/).
 
 `;
 }
